3d26344edca7809b994845245ef597203e363bad
jlsalaza3 <jlsalaza3@gmail.com>
2024-11-27 11:31:04 -0500

    se crea repositorio para crear backend proyecto integrador con relaciones muchos a muchos

---
+++ src/main/java/com/example/prueballaves/PrueballavesApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueballaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrueballavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrueballavesApplication.class, args);
+	}
+
+}
